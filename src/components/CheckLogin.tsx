92e131adab41f244aafa1a0a478709b728f4ff05
dhuruv bansal <dhuruvbansal99@gmail.com>
2025-03-28 16:08:57 +0530

    fixed login popup nonresponsive close button

--- src/components/CheckLogin.tsx
+++ src/components/CheckLogin.tsx
@@ -1,43 +1,52 @@
 "use client";
 import { getCookie } from "@/utility/getCookies";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 import { LogIn, UserPen } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
 export default function CheckLogin() {
-  const [refreshToken, setRefreshToken] = useState<string | null>();
+  const triggerRef = useRef<HTMLButtonElement>(null);
+
   const pathname = usePathname();
   const router = useRouter();
+
   useEffect(() => {
-    setRefreshToken(getCookie("refreshToken"));
+    const token = getCookie("refreshToken");
+    const dialogState = triggerRef.current && triggerRef.current.dataset["state"];
+    if ((!token && pathname !== "/login" && pathname !== "/register" && dialogState === "closed") || (pathname !== "/login" && pathname !== "/register" && dialogState === "open")) {
+      triggerRef.current?.click();
+    }
   }, [pathname]);
-  if (pathname === "/register" || pathname === "/login" || refreshToken) return <></>;
 
   return (
-    <Dialog open={true}>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Login To Continue</DialogTitle>
-          <DialogDescription>Please Login To your Accout OR Register if its your first time using the app to continue.</DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col items-center w-full gap-4 py-4">
-          <div className="flex items-center gap-4 w-fit">
-            <Button className="flex" onClick={() => router.push("/login")}>
-              Login
-              <LogIn />
-            </Button>
+    <>
+      {/* if production no annoying login popup */}
+      <Dialog>
+        <DialogTrigger ref={triggerRef}></DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Login To Continue</DialogTitle>
+            <DialogDescription>Please Login To your Accout OR Register if its your first time using the app to continue.</DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col items-center w-full gap-4 py-4">
+            <div className="flex items-center gap-4 w-fit">
+              <Button className="flex" onClick={() => router.push("/login")}>
+                Login
+                <LogIn />
+              </Button>
+            </div>
+            <div className="flex items-center gap-4">
+              <Button className="flex" onClick={() => router.push("/register")}>
+                Register
+                <UserPen />
+              </Button>
+            </div>
           </div>
-          <div className="flex items-center gap-4">
-            <Button className="flex" onClick={() => router.push("/register")}>
-              Register
-              <UserPen />
-            </Button>
-          </div>
-        </div>
-      </DialogContent>
-    </Dialog>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
